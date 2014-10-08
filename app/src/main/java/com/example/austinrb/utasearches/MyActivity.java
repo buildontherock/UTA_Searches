2713e64460ac2b1009516011b970e7b4ac6a3e89
utmaverick <austinrburkhart15@gmail.com>
2014-10-08 17:57:03 -0500

    UTA searches app

--- app/src/main/java/com/example/austinrb/utasearches/MyActivity.java
+++ app/src/main/java/com/example/austinrb/utasearches/MyActivity.java
@@ -123,6 +123,7 @@ public class MyActivity extends Activity {
         tagText.setText("");
 
         tagList.add(tag);
+        searchText.requestFocus();
         adapter.notifyDataSetChanged();
 
 
